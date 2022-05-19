d94299dcefe6c29d07a955f62e4a80021097f5d5
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-05-19 18:15:09 -0300

    initialize firebase app

---
+++ src/utils/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBdjw7-hNqt5YGiSw27Ezjr8VlcG0Bmbp0',
+  authDomain: 'quick-notes-26c52.firebaseapp.com',
+  projectId: 'quick-notes-26c52',
+  storageBucket: 'quick-notes-26c52.appspot.com',
+  messagingSenderId: '171813789663',
+  appId: '1:171813789663:web:163bb6acd2b66aee1dee6a',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
