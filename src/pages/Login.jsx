7c01efbf541da2a0b9f240f3066cd565677d7467
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-05-23 11:45:41 -0300

    add login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import Container from '../components/Container';
+import LoginForm from '../components/LoginForm';
 
 const Login = () => {
   return (
     <Container>
       <h1 className="text-lg font-bold">Login</h1>
+      <LoginForm />
     </Container>
   );
 };
