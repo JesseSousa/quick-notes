cf104e68f052c0cd588385e3e5803c3d8416e5a5
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-05-24 15:52:58 -0300

    add signUp form to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,15 @@
 import Container from '../components/Container';
 import LoginForm from '../components/LoginForm';
+import SignupForm from '../components/SignupForm';
 
 const Login = () => {
   return (
     <Container>
-      <h1 className="text-lg font-bold">Login</h1>
-      <LoginForm />
+      <div className="flex flex-col md:flex-row items-center justify-center h-full">
+        <LoginForm />
+        <div className="w-full h-px my-4 md:w-px bg-stone-300 md:h-full md:my-0 md:mx-2"></div>
+        <SignupForm />
+      </div>
     </Container>
   );
 };
