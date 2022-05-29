78700340a6682c30e491bfc84a85e6bbc3adb488
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-05-29 18:28:13 -0300

    change implementation and spec of createUserDoc
    New Spec:
      - No longer call toasts from within the function
      - Don't return anything, just throw errors

--- src/utils/firebase/helpers.js
+++ src/utils/firebase/helpers.js
@@ -1,29 +1,24 @@
 import { doc, setDoc, getDoc } from 'firebase/firestore';
 import { db } from './firebase';
-import { success, failure } from '../toasts';
 
+// don't return anything, just throw errors
 export const createUserDoc = async (userAuth) => {
-  try {
-    const { displayName, email, photoURL, uid } = userAuth;
+  const { displayName, email, photoURL, uid } = userAuth;
 
-    const userDocRef = doc(db, 'users', uid);
-    const userSnapshot = await getDoc(userDocRef);
-    if (userSnapshot.exists()) return;
+  const userDocRef = doc(db, 'users', uid);
+  const userSnapshot = await getDoc(userDocRef);
+  if (userSnapshot.exists()) return;
 
-    const USER_INITIAL_DATA = {
-      displayName,
-      email,
-      photoURL,
-      workspace: {
-        folders: [],
-        notes: [],
-      },
-      bio: '',
-    };
-    await setDoc(userDocRef, USER_INITIAL_DATA);
-    success('Signed up successfully');
-  } catch (error) {
-    console.error(error.message);
-    failure('Something went wrong! Try again later');
-  }
+  const USER_INITIAL_DATA = {
+    displayName,
+    email,
+    photoURL,
+    workspace: {
+      folders: [],
+      notes: [],
+    },
+    bio: '',
+  };
+
+  await setDoc(userDocRef, USER_INITIAL_DATA);
 };
