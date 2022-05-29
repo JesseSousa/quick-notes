a9f4991303f482d09cc9a191c3aaa5c6bd690243
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-05-29 18:45:35 -0300

    disable container if loading

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   return (
-    <Container>
+    <Container disabled={isLoading}>
       {isLoading && (
         <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] z-10">
           <LoadingAnimation />
